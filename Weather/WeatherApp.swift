934a104a3aa17e7bfe6830a0b74bee85752b3f25
arshad <ak849496@gmail.com>
2024-01-12 10:42:43 +0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Arshad Mustafa on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
